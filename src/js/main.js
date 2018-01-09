55b1fc358d287206545090bf500a5194d7ee3339
Ped <git@pedzed.com>
2018-01-09 13:30:46 +0100

    Fixed "is care taker coming" feature

--- src/js/main.js
+++ src/js/main.js
@@ -7,35 +7,84 @@ import config from './config';
         `ws://${config.server.ip}:${config.server.port}`
     );
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const $statusPage = $('#status-page');
-    const $residentPage = $('#resident-page');
+    let residents = [];
 
-    if (urlParams.has('resident_id')) {
-        if (urlParams.has('is_caretaker_coming')) {
-            sendCareTakerComingData(urlParams.get('resident_id'), urlParams.get('is_caretaker_coming'));
+    socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        residents = data.residents;
+
+        if (residents.length == 0) {
+            residents = [
+                {
+                    id: 1,
+                    name: "Bert Hendriks",
+                    address: "Adresje 17",
+                    section: "QPZ33.5",
+                    low_battery: true,
+                },
+                {
+                    id: 2,
+                    name: "Evert Klein",
+                    address: "Adresje 17",
+                    section: "JKP42.1",
+                },
+                {
+                    id: 3,
+                    name: "Celine Pepers",
+                    address: "Adresje 17",
+                    section: "DRJ21.2",
+                },
+                {
+                    id: 4,
+                    name: "Dirk de Haan",
+                    address: "Adresje 17",
+                    section: "QPZ33.5",
+                    is_fallen: true,
+                },
+                {
+                    id: 5,
+                    name: "Vanessa van Vliet",
+                    address: "Adresje 17",
+                    section: "LF94.5",
+                },
+            ];
         }
 
-        handleResidentPage(urlParams.get('resident_id'));
-    } else {
-        handleStatusPage();
-    }
+        const urlParams = new URLSearchParams(window.location.search);
+        const $statusPage = $('#status-page');
+        const $residentPage = $('#resident-page');
 
-    function sendCareTakerComingData(residentId, value) {
-        socket.send({
-            id: residentId,
-            is_coming: value,
-        });
-    }
+        if (urlParams.has('resident_id')) {
+            const residentId = urlParams.get('resident_id');
 
-    function handleResidentPage(residentId) {
-        $statusPage.hide();
-        $residentPage.show();
+            if (urlParams.has('is_caretaker_coming')) {
+                sendCareTakerComingData(
+                    residentId,
+                    urlParams.get('is_caretaker_coming')
+                );
 
-        socket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
+                setTimeout(() => {
+                    $(location).attr('href', `?resident_id=${residentId}`);
+                }, 2000);
+            } else {
+                handleResidentPage(residentId);
+            }
+        } else {
+            handleStatusPage();
+        }
 
-            let resident = $.map(data.residents, (resident) => {
+        function sendCareTakerComingData(residentId, value) {
+            socket.send(JSON.stringify({
+                id: new Number(residentId),
+                is_coming: (value == 'true') ? true : false,
+            }));
+        }
+
+        function handleResidentPage(residentId) {
+            $statusPage.hide();
+            $residentPage.show();
+
+            let resident = $.map(residents, (resident) => {
                 if (resident.id == residentId) {
                     return resident;
                 }
@@ -47,7 +96,7 @@ import config from './config';
                     name: "Bert Hendriks",
                     address: "Adresje 17",
                     section: "QPZ33.5",
-                    status: "BATTERY_LOW",
+                    low_battery: true,
                 };
             }
 
@@ -70,66 +119,21 @@ import config from './config';
             );
             $('#resident-page__status').html(statusHtml);
 
-            // If caretaker of resident is coming
-            if (resident.is_coming) {
-                $('#resident-page__on-the-way').html(
-                    `Yes. <a href="?resident_id=${resident.id}&is_caretaker_coming=false">zet uit</a>`
-                );
-            } else {
-                $('#resident-page__on-the-way').html(
-                    `No. <a href="?resident_id=${resident.id}&is_caretaker_coming=true">zet aan</a>`
-                );
+            if (resident.is_fallen) {
+                // If caretaker of resident is coming
+                if (resident.is_coming) {
+                    $('#resident-page__on-the-way').html(
+                        `<p>Onderweg.</p><a href="?resident_id=${resident.id}&is_caretaker_coming=false" class="button small">Zet op "niet onderweg"</a>`
+                    );
+                } else {
+                    $('#resident-page__on-the-way').html(
+                        `<p>Niet onderweg.</p><a href="?resident_id=${resident.id}&is_caretaker_coming=true" class="button small">Zet op "onderweg"</a>`
+                    );
+                }
             }
         };
-    };
 
-    function handleStatusPage() {
-        let residents = [];
-
-        socket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            residents = data.residents;
-
-            if (!residents) {
-                residents = [
-                    {
-                        id: 1,
-                        name: "Bert Hendriks",
-                        address: "Adresje 17",
-                        section: "QPZ33.5",
-                        status: "BATTERY_LOW",
-                    },
-                    {
-                        id: 2,
-                        name: "Evert Klein",
-                        address: "Adresje 17",
-                        section: "JKP42.1",
-                        status: "OK",
-                    },
-                    {
-                        id: 3,
-                        name: "Celine Pepers",
-                        address: "Adresje 17",
-                        section: "DRJ21.2",
-                        status: "OK",
-                    },
-                    {
-                        id: 4,
-                        name: "Dirk de Haan",
-                        address: "Adresje 17",
-                        section: "QPZ33.5",
-                        status: "POSSIBLY_FALLEN",
-                    },
-                    {
-                        id: 5,
-                        name: "Vanessa van Vliet",
-                        address: "Adresje 17",
-                        section: "LF94.5",
-                        status: "OK",
-                    },
-                ];
-            }
-
+        function handleStatusPage() {
             const $residentsTableRows = $('#residents-table-rows');
             $residentsTableRows.empty();
 
@@ -145,10 +149,10 @@ import config from './config';
 
                 let statusHtml = `<i class="far fa-circle fa-xs" style="color: limegreen;"></i> In orde`;
 
-                if (resident.status == 'BATTERY_LOW') {
-                    statusHtml = `<i class="fa fa-circle fa-xs" style="color:orange;"></i> Batterij laag`;
-                } else if (resident.status == 'POSSIBLY_FALLEN') {
-                    statusHtml = `<i class="fa fa-circle fa-xs" style="color:red;"></i> Mogelijk gevallen`;
+                if (resident.low_battery) {
+                    statusHtml = `<i class="fa fa-circle fa-xs" style="color: orange;"></i> Batterij laag`;
+                } else if (resident.is_fallen) {
+                    statusHtml = `<i class="fa fa-circle fa-xs" style="color: red;"></i> Mogelijk gevallen`;
                 }
 
                 $tr.append($('<td>').html(statusHtml));
@@ -156,11 +160,6 @@ import config from './config';
 
                 $tr.appendTo($residentsTableRows);
             });
-
-            setInterval(() => {
-                socket.send('Ping!');
-                console.log('Pinged to server.');
-            }, 1000);
-        };
+    };
     };
 })(jQuery);
